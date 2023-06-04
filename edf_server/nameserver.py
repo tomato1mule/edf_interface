b26ea175e4b99b8c8c7cc982d7ed220becd5fcc3
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-06-04 18:07:17 +0900

    nameserver

---
+++ edf_server/nameserver.py
@@ -0,0 +1,53 @@
+from typing import Optional, Union
+import threading
+import logging
+import time
+
+from beartype import beartype
+from Pyro5.nameserver import start_ns, NameServerDaemon, BroadcastServer
+from Pyro5.core import URI
+
+
+class NameServer():
+    nsUri: Optional[URI] = None
+    daemon: Optional[NameServerDaemon] = None
+    bcserver: Optional[BroadcastServer] = None
+    _ns_thread: Optional[threading.Thread] = None
+    log: logging.Logger
+
+
+    @beartype
+    def __init__(self):
+        self.log = logging.getLogger("NameServer")
+        self.init_nameserver()
+        
+
+    def init_nameserver(self, *args, **kwargs):
+        self.nsUri, self.daemon, self.bcserver = start_ns(*args, **kwargs)
+        self._ns_thread = threading.Thread(target=self._ns_daemon_request_loop)
+        self._ns_thread.start()
+
+    def _ns_daemon_request_loop(self):
+        try:
+            self.daemon.requestLoop()
+            self.log.debug(f"Nameserver daemon loop exit")
+        finally:
+            self.daemon.close()
+
+    def close(self, timeout = 5.) -> bool:
+        self.log.warning(f"Closing Nameserver... (Timeout: {timeout} sec)")
+        init_time = time.time()
+        self.daemon.close()
+        if self.bcserver is not None:
+            self.bcserver.close()
+        self._ns_thread.join(timeout=timeout)
+        
+        if self._ns_thread.is_alive():
+            self.log.error(f"Nameserver daemon loop process not cleanly closed in {time.time() - init_time} seconds.")
+            return False
+        else:
+            self.daemon.close()
+            if self.bcserver is not None:
+                self.bcserver.close()
+            self.log.warning(f"Nameserver daemon loop process cleanly closed in {time.time() - init_time} seconds.")
+            return True
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="edf_server",
+    version="0.0.1",
+    author="Hyunwoo Ryu",
+    author_email="tomato1mule@gmail.com",
+    description="Network interface for EDFs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomato1mule/edf_server",
+    project_urls={
+        "Bug Tracker": "https://github.com/tomato1mule/edf_server/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Ubuntu 22.04",
+    ],
+    packages=['edf_server'],
+    python_requires="<3.9",
+    install_requires=[
+        'beartype',
+        'Pyro5==5.14',
+    ]
+)
