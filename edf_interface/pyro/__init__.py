1e9c2885119a8c1fc0787d5f4b8bfa8bc97d4c3d
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-07-26 00:29:40 +0900

    traceback

--- edf_interface/pyro/__init__.py
+++ edf_interface/pyro/__init__.py
@@ -2,6 +2,7 @@ import pickle
 
 import Pyro5
 Pyro5.config.SERPENT_BYTES_REPR = True
+Pyro5.config.DETAILED_TRACEBACK = True
 import Pyro5.api
 from .nameserver import NameServer
 from .server import PyroServer
